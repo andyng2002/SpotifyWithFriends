0ff318d9ebc6f7b244fce2aaa508f266c1b4038d
tylerstennett <tyler.j.stennett@gmail.com>
2022-10-21 23:36:25 -0400

    Original Index Files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/app/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules:[{
+            test: /\.jsx?/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: '/index.html'
+    })],
+};
