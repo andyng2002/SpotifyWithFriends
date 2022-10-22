ada0f90b849f09be51c05f873f8e510055198f6c
tylerstennett <tyler.j.stennett@gmail.com>
2022-10-22 13:31:18 -0400

    Beginning of CORS request

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const app = express();
 const port = 8888;
 
-
 var SpotifyWebApi = require('spotify-web-api-node');
 let scopes = ['playlist-modify-private', 'playlist-modify-public', 'user-top-read', 'user-read-email', 'ugc-image-upload'];
 
@@ -18,15 +17,11 @@ app.listen(port, () => {
     console.log("App is listening");
 });
 
-router.get('/', (req, res) => {
-    /*res.send(
-        /!*"<a href='https://accounts.spotify.com/authorize?client_id=" + e2d4a4b1eedf493f86413b3a31892213 +
-        "&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&scope=user-top-read'>Sign in</a>"*!/
-        "Hello Pie!"
-    );*/
+router.get('/clicked', (req, res) => {
     var html = spotifyApi.createAuthorizeURL(scopes) + "&show_dialog=true"
     console.log(html)
-    res.send("<a href='" + html + "'> Sign In </a>")
+    //res.send("<a href='" + html + "'> Sign In </a>")
+    res.redirect(html);
 });
 
 app.use(router);
