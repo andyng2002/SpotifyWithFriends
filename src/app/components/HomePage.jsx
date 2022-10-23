44066597c3bfe6083e63e00f0f4b08d846082a77
Andy Nguyen <andyng2002@gmail.com>
2022-10-22 22:12:19 -0400

    fix navbar

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { Group } from './Group';
 
 export const HomePage = () => {
-
+    console.log("asd")
     return(
         <div>
             <Group value="id1"></Group>
-            
         </div>
     )
 }
--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -6,10 +6,12 @@ import { HomePage } from './HomePage';
 import { Link, Route, Routes } from "react-router-dom"
 //import { Router, Route } from 'react-router-dom';
 import { Group } from './Group';
+import { NavBar } from './navbar';
 
 function Main() {
     return(
         <>
+            <NavBar></NavBar>
             <Routes>
                 <Route exact path="/" element = {<LandingPage />} />
                 <Route path = "/home" element = {<HomePage />} />
