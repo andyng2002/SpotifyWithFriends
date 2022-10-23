9dd03f926372b6394038de69c033fa3f3ad9e906
Aiden Perez <aperez308@gatech.edu>
2022-10-22 21:58:09 -0400

    add

--- src/app/components/Group.jsx
+++ src/app/components/Group.jsx
@@ -1,15 +1,16 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import { Typography } from '@mui/material'
+import PropTypes from 'prop-types'
 import profile from './profile.png';
 
-export const Group = () => {
+export const Group = ({ title, pic, members }) => {
 
 
     return (
         <Box>
             <Box>
-                <img src={profile} alt='profile' />
+                <img src={pic} alt='profile' />
             </Box>
         </Box>
     )
